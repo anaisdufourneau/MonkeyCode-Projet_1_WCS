45d6ad1a36afe1d04a5b73d7b578c90176a62caa
Rossignol Alex <alexnw33910@gmail.com>
2024-06-06 10:07:33 +0200

    Js welcome

--- JavaScriptFiles/scriptWelcome.js
+++ JavaScriptFiles/scriptWelcome.js
@@ -1,15 +1,22 @@
-const btn = document.querySelector('.btn-confirm');
+// LOGO 
+
+const logoMonkey = document.querySelector('.logo-monkey');
+
+    if(window.innerWidth >= 992) {
+        logoMonkey.src = 'Images/logoForDesktop.png';
+    };
+    
+    // BUTTON -> next page
+
+const btn = document.querySelector('#btn-confirm');
 const firstName = document.querySelector('.firstname');
 const lastName = document.querySelector('.lastname');
 
 btn.addEventListener('click', () => {
-    localStorage.setItem('firstName' , firstName.value)
-    localStorage.setItem('lastName' , lastName.value)
-})
+    localStorage.setItem('firstName' , firstName.value);
+    localStorage.setItem('lastName' , lastName.value);
+});
 
 
-// LOGO 
-    const logoMonkey = document.querySelector('.logo-monkey');
-    if (window.innerWidth >= 992) {
-        logoMonkey.src = 'Images/logoForDesktop.png';
-    } 
+
+
